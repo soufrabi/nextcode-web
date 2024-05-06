a6975c79c9ce76105391341a2237982ac0b555cb
Anirban Dey <anirbandey@soufrabi.com>
2024-05-06 19:04:24 +0530

    make images buttons

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -9,38 +9,48 @@ function NavBar() {
         <div className="bg-slate-100 w-full px-6">
             <nav className="flex justify-between items-center mx-auto bg-[f0f0f0] h-12">
                 <div className="flex items-center gap-4">
-                    <Image
-                        src="/leetcode-logo.png"
-                        alt="Leetcode Logo"
-                        width={20}
-                        height={20}
-                    />
+                    <button>
+                        <Image
+                            src="/leetcode-logo.png"
+                            alt="Leetcode Logo"
+                            width={20}
+                            height={20}
+                        />
+                    </button>
                     <div className="flex gap-3 items-center">
-                        <Image
-                            src="/playlist-symbolic.svg"
-                            alt="problem list"
-                            width={20}
-                            height={20}
-                        />
+                        <button>
+                            <Image
+                                src="/playlist-symbolic.svg"
+                                alt="problem list"
+                                width={20}
+                                height={20}
+                            />
+                        </button>
                         <span>Problem List</span>
-                        <Image
-                            src="/left-small-symbolic.svg"
-                            alt="previous problem"
-                            width={20}
-                            height={20}
-                        />
-                        <Image
-                            src="/right-small-symbolic.svg"
-                            alt="next problem"
-                            width={20}
-                            height={20}
-                        />
-                        <Image
-                            src="/playlist-shuffle-symbolic.svg"
-                            alt="previous problem"
-                            width={18}
-                            height={18}
-                        />
+                        <button>
+                            <Image
+                                src="/left-small-symbolic.svg"
+                                alt="previous problem"
+                                width={20}
+                                height={20}
+                            />
+                        </button>
+                        <button>
+                            <Image
+                                src="/right-small-symbolic.svg"
+                                alt="next problem"
+                                width={20}
+                                height={20}
+                            />
+                        </button>
+                        <button>
+                            <Image
+                                src="/playlist-shuffle-symbolic.svg"
+                                alt="previous problem"
+                                width={18}
+                                height={18}
+                            />
+                        </button>
                     </div>
                 </div>
                 <div className="flex gap-4">
