eaef6eb347f24ac1701cd4a61b840219c9812da0
Anirban Dey <anirbandey@soufrabi.com>
2024-05-16 16:08:10 +0530

    add tag and hint disclosures(accordians)

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -1,14 +1,77 @@
 "use client";
 
+import React from "react";
 import Image from "next/image";
 import { useParams } from "next/navigation"
 import { Editor } from "@monaco-editor/react";
+import { nanoid } from "nanoid";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { TagIcon } from "@heroicons/react/24/outline";
+import { LightBulbIcon } from "@heroicons/react/24/outline";
+
+
+function TopicsDisclosure(props: { topics: string[] }) {
+    return (
+        <div>
+            <Disclosure>
+                {({ open }) => (
+                    <>
+                        <DisclosureButton className="w-full flex justify-between">
+                            <div className="flex gap-1 items-center">
+                                <TagIcon className="h-4 w-4 mr-1" />
+                                <span className="text-sm">Topics</span>
+                            </div>
+                            <ChevronDownIcon className={`${open ? 'rotate-180 transform' : ''} h-5 w-5`} />
+                        </DisclosureButton>
+                        <DisclosurePanel className="">
+                            <div className="pl-4 pt-3 flex gap-2">
+                                {props.topics.map((topicName: string) => (
+                                    <span key={nanoid()} className="bg-slate-100 text-xs px-2 py-1">{topicName}</span>
+                                ))
+                                }
+                            </div>
+                        </DisclosurePanel>
+                    </>
+
+                )}
+            </Disclosure>
+        </div>
+    )
+}
+
+
+function HintsDisclosure(props: { index: number, bodyText: string }) {
+    return (
+        <div>
+            <Disclosure>
+                {({ open }) => (
+                    <>
+                        <DisclosureButton className="w-full flex justify-between">
+                            <div className="flex gap-1 items-center">
+                                <LightBulbIcon className="h-4 w-4 mr-1" />
+                                <span className="text-sm">Hint {props.index + 1}</span>
+                            </div>
+                            <ChevronDownIcon className={`${open ? 'rotate-180 transform' : ''} h-5 w-5`} />
+                        </DisclosureButton>
+                        <DisclosurePanel className="">
+                            <div className="pl-4 pt-3 flex gap-2">
+                                <span className="text-xs px-2 py-1">{props.bodyText}</span>
+                            </div>
+                        </DisclosurePanel>
+                    </>
+
+                )}
+            </Disclosure>
+        </div>
+    )
+}
 
 function LeftPart() {
     const problemNumber = "1"
     const problemName = "Two Sum"
     return (
-        <div className="p-2" style={{ height: "95vh" }} >
+        <div className="p-2" style={{ height: "100%" }} >
             <div className="bg-slate-100 p-2">
                 <div className="flex gap-4 pb-2 px-1">
                     <button className="flex gap-1 items-center">
@@ -49,7 +112,7 @@ function LeftPart() {
                     </button>
 
                 </div>
-                <div className="bg-white p-4">
+                <div className="bg-white p-4" >
                     <span className="text-2xl font-semibold">{problemNumber}. {problemName}</span>
 
                     <div className="text-sm pt-4">
@@ -60,18 +123,61 @@ function LeftPart() {
                     </div>
 
                     <div>
-                        <span className="font-semibold">Example 1:</span> <br/>
-                        <div className="border-l-[1px] border-l-gray-300 pl-4 mt-4">
-                            <span className="font-medium">Input: </span> <span className="text-sm text-gray-600"> nums = [2,7,11,15], target = 9 </span> <br />
-                            <span className="font-medium">Output: </span> <span className="text-sm text-gray-600"> [0,1] </span> <br />
-                            <span className="font-medium">Explanation: </span> <span className="text-sm text-gray-600"> Because nums[0] + nums[1] == 9, we return [0, 1]. </span> <br />
+                        <span className="font-semibold">Example 1:</span> <br />
+                        <div className="border-l-[1px] border-l-gray-300 pl-4 my-2">
+                            <span className="font-medium text-sm">Input: </span> <span className="text-sm text-gray-600"> nums = [2,7,11,15], target = 9 </span> <br />
+                            <span className="font-medium text-sm">Output: </span> <span className="text-sm text-gray-600"> [0,1] </span> <br />
+                            <span className="font-medium text-sm">Explanation: </span> <span className="text-sm text-gray-600"> Because nums[0] + nums[1] == 9, we return [0, 1]. </span> <br />
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="font-semibold">Constraints:</span>
+                        <div className="pl-4 my-2">
+                            <ul className="text-sm list-disc">
+                                <li> <span className="bg-slate-100 px-2 py-1"> {`2 <= nums.length <= 104`} </span> </li>
+                                <li> <span className="bg-slate-100 px-2 py-1"> {`-109 <= nums[i] <= 109`} </span>  </li>
+                                <li> <span className="bg-slate-100 px-2 py-1" > {`-109 <= target <= 109`} </span> </li>
+                                <li> <span className="font-semibold px-2 py-1"> {`Only one valid answer exists.`} </span> </li>
+                            </ul>
                         </div>
 
                     </div>
 
+                    <div className="mt-8 flex gap-4">
+                        <div>
+                            <span className="text-gray-500 text-xs">Accepted:</span> <span className="text-sm">13.2M</span>
+                        </div>
+                        <div className="border-r-gray-200 border-r-2" />
+                        <div>
+                            <span className="text-gray-500 text-xs">Submissions:</span> <span className="text-sm">25M</span>
+                        </div>
+                        <div className="border-r-gray-200 border-r-2" />
+                        <div>
+                            <span className="text-gray-500 text-xs">Acceptance Rate:</span> <span className="text-sm">52.5%</span>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-6">
+                        <div className="w-full border-b-slate-100 border-b-2"> </div>
+
+                        <div className="w-full border-b-slate-100 border-b-2 pb-3">
+                            <TopicsDisclosure topics={["Array", "2-pointer"]} />
+                        </div>
+
+
+                        <div className="w-full border-b-slate-100 border-b-2 pb-3">
+                            <HintsDisclosure index={0} bodyText={`A really brute force way would be to search for all possible pairs of numbers but that would be too slow. Again, it's best to try out brute force solutions for just for completeness. It is from these brute force solutions that you can come up with optimizations`} />
+                        </div>
+
+                        <div className="w-full border-b-slate-100 border-b-2 pb-3">
+                            <HintsDisclosure index={1} bodyText={`The second train of thought is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?`} />
+                        </div>
+                    </div>
+
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
@@ -113,7 +219,51 @@ int main(int argc, char** argv) {
     )
 }
 
+function CaseButton(props: { id: number, index: number, removeTestCase: any }) {
+    const [closeButtonVisible, setCloseButtonVisible] = React.useState<boolean>(false)
+    const handleMouseEnter = () => {
+        setCloseButtonVisible(true)
+    }
+
+    const handleMouseLeave = () => {
+        setCloseButtonVisible(false)
+    }
+
+    return (
+        <div key={nanoid()} className="bg-slate-100 p-2 relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <span>
+                Case {props.index + 1}
+            </span>
+            <div className="absolute -top-1 -right-1 bg-gray-50 rounded-full hover:bg-slate-300 ">
+
+                <Image
+                    src="/close-icon-1.png"
+                    alt="editorial"
+                    width={10}
+                    height={10}
+                    onClick={() => { props.removeTestCase(props.id) }}
+                    style={
+                        { display: closeButtonVisible ? "block" : "none" }
+                    }
+                />
+            </div>
+        </div>
+
+    )
+
+}
+
 function TestCasePanel() {
+    const [caseList, setCaseList] = React.useState<number[]>([1, 2, 3])
+
+    const addTestCase = () => {
+        setCaseList((prevList: number[]) => [...prevList, prevList.length + 1])
+    }
+
+    const removeTestCase = (id: number) => {
+        setCaseList((prevList: number[]) => prevList.filter((item) => item != id))
+    }
+
     return (
         <div className="p-2">
             <div className="bg-slate-100 flex gap-4 py-2 px-2">
@@ -138,8 +288,36 @@ function TestCasePanel() {
                 </button>
 
             </div>
-            <div className="bg-white">
-                Test Case Panel
+            <div className="bg-white p-4 ">
+                <div className="flex gap-2">
+                    {
+                        caseList.map((caseButtonId: number, index: number) => {
+                            return (
+                                <CaseButton key={nanoid()} id={caseButtonId} index={index} removeTestCase={removeTestCase} />
+                            )
+                        })
+                    }
+
+                    <button className="bg-slate-50 p-2" onClick={addTestCase}>
+                        +
+                    </button>
+                </div>
+
+                <div>
+                </div>
+
+                <div>
+
+                    <button className="flex gap-1 items-center">
+                        <Image
+                            src="/coding-icon.png"
+                            alt="source-mode"
+                            width={20}
+                            height={20}
+                        />
+                        <span className="text-base text-gray-600"> Source </span>
+                    </button>
+                </div>
             </div>
         </div>
     )
@@ -155,12 +333,18 @@ function RightPart() {
 
 
 function NavBar() {
+    const params = useParams<{ id: string }>()
+
+    const handleContextMenu = (ev: any) => {
+        ev.preventDefault()
+        window.open(`https://leetcode.com/problems/${params.id}`, "_blank")
+    }
+
     return (
-        // <nav className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee]" w-[92%]>
         <div className="w-full px-6">
             <nav className="flex justify-between items-center mx-auto bg-[f0f0f0] h-12">
                 <div className="flex items-center gap-4">
-                    <button>
+                    <button onContextMenu={handleContextMenu}>
                         <Image
                             src="/leetcode-logo.png"
                             alt="Leetcode Logo"
@@ -279,12 +463,14 @@ function NavBar() {
 }
 
 export default function ProblemPage() {
-    const params = useParams<{ id: string }>()
+
+    React.useEffect(() => {
+        // document.body.style.overflow = "hidden"
+    }, [])
 
     return (
         <main className="bg-slate-200">
             <NavBar />
-            {/* Leetcode ProblemPage {params.id} */}
             <div className="w-full grid grid-cols-2 gap-0">
                 <LeftPart />
                 <RightPart />
