8eefba54a343f93bb785eb765b5679f1694c1955
Anirban Dey <anirbandey@soufrabi.com>
2024-05-21 22:55:00 +0530

    Add ListBox to change "Programming Language"

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -6,6 +6,16 @@ import { Editor } from "@monaco-editor/react"
 import Split from "react-split"
 import { FaPlay } from "react-icons/fa"
 import axios, { AxiosResponse } from "axios"
+// import { nanoid } from "nanoid"
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/react"
+import { HiChevronDown } from "react-icons/hi2"
+
+import { ToastContainer } from 'react-toastify'
+// import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+// import { HiCheck } from 'react-icons/hi2'
+// import { HiChevronDown } from 'react-icons/hi2'
 
 function NavBar() {
     return (
@@ -29,10 +39,77 @@ function NavBar() {
 
 }
 
-function CodeEditor(props: { sourceCodeValue: string, setSourceCodeValue: React.Dispatch<React.SetStateAction<string>>, runCodeAction: ()=>Promise<void> }) {
-    const pythonDefaultValue = `
-print("Hello World from Python")
-`
+type ProgrammingLanguage = {
+    id: number,
+    name: string,
+    defaultCode: string,
+    available: boolean,
+}
+
+const defaultCodeText = "Write your code here ..."
+
+const languageList: Array<ProgrammingLanguage> = [
+    { id: 1, name: 'Python 3.11', defaultCode: `# ${defaultCodeText}`, available: true },
+    { id: 2, name: 'C++ 23', defaultCode: `// ${defaultCodeText}`, available: false },
+    { id: 3, name: 'Java 17', defaultCode: `// ${defaultCodeText}`, available: true },
+    { id: 4, name: 'Go 1.21', defaultCode: `// ${defaultCodeText}`, available: true },
+]
+
+function LanguageSelector(
+    props: {
+        selectedLanguage: ProgrammingLanguage,
+        setSelectedLanguage: React.Dispatch<React.SetStateAction<ProgrammingLanguage>>
+    }
+) {
+
+
+    return (
+        <div className="">
+            <Listbox
+                value={props.selectedLanguage}
+                onChange={props.setSelectedLanguage}
+            >
+                <ListboxButton
+                    className={"p-2 border-gray-200/75 border-2 text-sm flex flex-row gap-2 items-center hover:shadow-customhovereffect rounded-lg"}
+                >
+                    <span>
+                        {props.selectedLanguage.name}
+                    </span>
+                    <HiChevronDown
+                        width={5}
+                        height={5}
+                    />
+                </ListboxButton>
+                <ListboxOptions
+                    anchor="bottom start"
+                    className={"text-sm shadow-lg bg-white rounded-lg"}
+                >
+                    {
+                        languageList.map((language) => (
+                            <ListboxOption
+                                key={language.id}
+                                value={language}
+                                disabled={!language.available}
+                                className={"p-2 hover:shadow-customhovereffect"}
+                            >
+                                <span>{language.name}{language.available ? "" : " (Coming Soon)"}</span>
+                            </ListboxOption>
+
+                        ))
+                    }
+                </ListboxOptions>
+
+
+            </Listbox>
+        </div>
+    )
+}
+
+
+
+
+function CodeEditor(props: { sourceCodeValue: string, setSourceCodeValue: React.Dispatch<React.SetStateAction<string>>, runCodeAction: (language: ProgrammingLanguage) => Promise<void> }) {
+    const [selectedLanguage, setSelectedLanguage] = React.useState<ProgrammingLanguage>(languageList[2])
 
     const handleChange = (newValue: string | undefined) => {
 
@@ -43,19 +120,15 @@ print("Hello World from Python")
     }
 
     const handleRunCodeButtonClicked = async () => {
-        await props.runCodeAction()
+        await props.runCodeAction(languageList[0])
     }
-
-    React.useEffect(() => {
-        props.setSourceCodeValue(pythonDefaultValue)
-    }, [])
 
     return (
 
         <div
             className="min-h-4 p-4 pt-0 h-[calc(100vh-7rem)]"
         >
-            <div className="pb-2">
+            <div className="pb-2 flex flex-row justify-between">
                 <button
                     className="bg-green-200 px-4 py-2 rounded-2xl flex flex-row gap-2 items-center hover:shadow-customhovereffect"
                     onClick={handleRunCodeButtonClicked}
@@ -67,12 +140,16 @@ print("Hello World from Python")
                         Run
                     </span>
                 </button>
+                <LanguageSelector
+                    selectedLanguage={selectedLanguage}
+                    setSelectedLanguage={setSelectedLanguage}
+                />
 
             </div>
             <Editor
                 // height="80vh"
                 defaultLanguage="python"
-                defaultValue={pythonDefaultValue}
+                // defaultValue={selectedLanguage.defaultCode}
                 value={props.sourceCodeValue}
                 onChange={handleChange}
                 options={{
@@ -82,10 +159,11 @@ print("Hello World from Python")
                     },
                     suggest: {
                         showWords: false,
-                    }
+                    },
+                    contextmenu: false,
                 }}
 
-                className="min-h-4 border-black border-2"
+                className="min-h-4 border-gray-300 border-2"
             />
         </div>
     )
@@ -166,11 +244,12 @@ export default function EditorPage() {
     const [stdoutValue, setStdoutValue] = React.useState<string>("")
     const [stderrValue, setStderrValue] = React.useState<string>("")
 
-    const runCodeAction = async () => {
+    const runCodeAction = async (language: ProgrammingLanguage) => {
         const bodyObj = {
             sourceCode: sourceCodeValue,
             inputText: inputTextValue,
             timeLimit: 3000,
+            language: language.name,
         }
 
         try {
@@ -238,6 +317,7 @@ export default function EditorPage() {
 
                 </Split>
             </div>
+            <ToastContainer />
         </main>
     )
 
