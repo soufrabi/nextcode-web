3e19fa170a382da5c6ac89d5f18ed79975ac2d24
Anirban Dey <anirbandey@soufrabi.com>
2024-06-16 21:23:39 +0530

    Use component NavBar defined in separate file

--- src/app/problemset/page.tsx
+++ src/app/problemset/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import clsx from "clsx"
 import { nanoid } from "nanoid"
 import Link from "next/link"
+import { NavBar } from "../components/NavBar"
 
 enum ProblemDifficultData {
     EASY,
@@ -20,84 +21,6 @@ type ProblemData = {
     frequency: string,
     titleSlug: string,
 
-}
-
-function NavBar() {
-    return (
-        <div className="w-full h-12 flex flex-row justify-around py-3 shadow-sm">
-            <div className="flex flex-row gap-6 items-center">
-                <Link
-                    href={"/"}
-                    className="cursor-pointer"
-                    onContextMenu={(ev: React.MouseEvent) => { ev.preventDefault() }}
-                >
-                    <Image
-                        src={"/nextcode-logo.png"}
-                        alt=""
-                        width={25}
-                        height={25}
-                        className="rounded-2xl"
-                    />
-                </Link>
-                <Link
-                    href={"/"}
-                    className="cursor-pointer"
-                >
-                    <span
-                        className="text-gray-500"
-                    >Explore</span>
-                </Link>
-                <Link
-                    href={"/problemset"}
-                    className="cursor-pointer"
-                >
-                    <span
-                        className="text-black"
-                    >Problems</span>
-                </Link>
-                <Link
-                    href={"/"}
-                    className="cursor-pointer"
-                >
-                    <span
-                        className="text-gray-500"
-                    >Contest</span>
-                </Link>
-                <Link
-                    href={"/"}
-                    className="cursor-pointer"
-                >
-                    <span
-                        className="text-gray-500"
-                    >Discuss</span>
-                </Link>
-                <Link
-                    href={"/"}
-                    className="cursor-pointer"
-                >
-                    <span
-                        className="text-gray-500"
-                    >Interview</span>
-                </Link>
-                <Link
-                    href={"/"}
-                    className="cursor-pointer"
-                >
-                    <span
-                        className="text-gray-500"
-                    >Store</span>
-                </Link>
-            </div>
-            <div className="flex flex-row items-center">
-                <div className="cursor-pointer">
-                    <span
-                        className="text-sm text-gray-600"
-                    >Sign in</span>
-                </div>
-            </div>
-
-        </div>
-    )
 }
 
 
