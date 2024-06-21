9018ca0b7309838aa4a34c02d3e889dd03f0811c
Anirban Dey <anirbandey@soufrabi.com>
2024-06-21 18:58:21 +0530

    Add boxshadow customalldirectionsmd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      boxShadow: {
-        "customhovereffect": "inset 0 0 100rem rgba(0,0,0,0.05), 0 0 100rem rgba(0,0,0,0.5)"
-      }
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            boxShadow: {
+                "customhovereffect": "inset 0 0 100rem rgba(0,0,0,0.05), 0 0 100rem rgba(0,0,0,0.5)",
+                "customalldirectionmd": "0 0 10px 0 rgb(0 0 0 / 0.1), 0 0 6px 0px rgb(0 0 0 / 0.1)",
+
+            }
+        },
     },
-  },
-  darkMode:"selector",
-  plugins: [],
+    darkMode: "selector",
+    plugins: [],
 };
 export default config;
