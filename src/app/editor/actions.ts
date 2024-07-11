9bed7854b207c32008d657c1963e8125ff7116ca
Anirban Dey <anirbandey@soufrabi.com>
2024-07-12 01:39:12 +0530

    Create server action to get boiler plate list
    for the selected language

--- src/app/editor/actions.ts
+++ src/app/editor/actions.ts
@@ -1,16 +1,30 @@
 "use server"
 
 import axios from "axios"
-import { ProgrammingLanguage } from "../data/editor"
+import { BoilerPlate, ProgrammingLanguage } from "../data/editor"
 
-export async function getProgrammingLanguages() : Promise<Array<ProgrammingLanguage>>{
+export async function getProgrammingLanguages(): Promise<Array<ProgrammingLanguage>> {
     try {
         const res = await axios.get(`${process.env.API_SERVER_URL}/editor/language/all`)
-        const data:Array<ProgrammingLanguage> = res.data
+        const data: Array<ProgrammingLanguage> = res.data
         console.log("Programming Language", data)
         return data
     } catch (err) {
-        console.error("Failed to get data")
+        console.error("failed to get programming language list")
         return []
     }
+}
+
+export async function getBoilerPlateList(languageId: number): Promise<Array<BoilerPlate>> {
+    try {
+        const res = await axios.get(`${process.env.API_SERVER_URL}/editor/boilerplate/${languageId}`)
+        const data: Array<BoilerPlate> = res.data
+        console.log("Boiler Plate List", data)
+        return data
+
+    } catch (err) {
+        console.error("failed to get boiler plate")
+        return []
+    }
+
 }
