4b8d4e0ca8965543a6acf7c1a2886cfb53c1d25b
Anirban Dey <anirbandey@soufrabi.com>
2024-07-12 09:58:59 +0530

    Set revalidate = 0 to generate content dynamically
    To avoid fetch requests to api server during docker builds

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,8 +1,8 @@
-"use server"
-
 import PlaygroundPreferencesProvider from "../store/PlaygroundPreferencesProvider"
 import { EditorPageClient } from "./EditorPageClient"
 import { getProgrammingLanguages } from "./actions"
+
+export const revalidate = 0
 
 export default async function EditorPage() {
     const programmingLanguageList = await getProgrammingLanguages()
