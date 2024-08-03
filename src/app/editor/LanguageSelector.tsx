8792eb14f3ce2623ec71dc883c04fd01428a3a03
Anirban Dey <anirbandey@soufrabi.com>
2024-08-03 11:56:58 +0530

    Remove outermost div in LanguageSelector, add theme based tailwind classes

--- src/app/editor/LanguageSelector.tsx
+++ src/app/editor/LanguageSelector.tsx
@@ -18,44 +18,42 @@ export function LanguageSelector({
 
 
     return (
-        <div className="">
-            <Listbox
-                value={selectedLanguage}
-                onChange={setSelectedLanguage}
+        <Listbox
+            value={selectedLanguage}
+            onChange={setSelectedLanguage}
+        >
+            <ListboxButton
+                className={"bg-white dark:bg-black dark:text-white px-2 py-1.5 border-gray-200/75 border-2 text-sm flex flex-row gap-2 items-center hover:shadow-customhovereffect rounded-lg"}
             >
-                <ListboxButton
-                    className={"p-2 border-gray-200/75 border-2 text-sm flex flex-row gap-2 items-center hover:shadow-customhovereffect rounded-lg"}
-                >
-                    <span>
-                        {selectedLanguage.name}
-                    </span>
-                    <HiChevronDown
-                        width={5}
-                        height={5}
-                    />
-                </ListboxButton>
-                <ListboxOptions
-                    anchor="bottom start"
-                    className={"text-sm shadow-lg bg-white dark:bg-black dark:text-white rounded-lg z-20"}
-                >
-                    {
-                        programmingLanguageList.map((language) => (
-                            <ListboxOption
-                                key={language.id}
-                                value={language}
-                                disabled={!language.available}
-                                className={"p-2 cursor-pointer hover:shadow-customhovereffect"}
-                            >
-                                <span>{language.name}{language.available ? "" : " (Coming Soon)"}</span>
-                            </ListboxOption>
+                <span>
+                    {selectedLanguage.name}
+                </span>
+                <HiChevronDown
+                    width={5}
+                    height={5}
+                />
+            </ListboxButton>
+            <ListboxOptions
+                anchor="bottom start"
+                className={"text-sm shadow-lg bg-white dark:bg-black dark:text-white rounded-lg z-20"}
+            >
+                {
+                    programmingLanguageList.map((language) => (
+                        <ListboxOption
+                            key={language.id}
+                            value={language}
+                            disabled={!language.available}
+                            className={"p-2 cursor-pointer hover:shadow-customhovereffect"}
+                        >
+                            <span>{language.name}{language.available ? "" : " (Coming Soon)"}</span>
+                        </ListboxOption>
 
-                        ))
-                    }
-                </ListboxOptions>
+                    ))
+                }
+            </ListboxOptions>
 
 
-            </Listbox>
-        </div>
+        </Listbox>
     )
 }
 
