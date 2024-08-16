5918121f8f20e2967d243d8d81f1cfeec999083a
Anirban Dey <anirbandey@soufrabi.com>
2024-08-16 19:58:40 +0530

    Use ProblemProvider in page.tsx of /problems/[id]

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use server"
 
+import ProblemProvider from "@/app/providers/ProblemProvider"
 import { DocumentBodyOverflow } from "./DocumentBodyOverflow"
 import ProblemPageClient from "./ProblemPageClient"
 import { getProgrammingLanguages, getTestCaseDefaultList } from "./actions"
@@ -16,12 +17,16 @@ export default async function ProblemPage({ params }: ProblemPageProps) {
     const programmingLanguageList = await getProgrammingLanguages(id)
 
     return (
-        <DocumentBodyOverflow>
-            <ProblemPageClient
-                problemId={id}
-                testCaseDefaultList={testCaseDefaultList}
-                programmingLanguageList={programmingLanguageList}
-            />
-        </DocumentBodyOverflow>
+        <ProblemProvider
+            testCaseDefaultList={testCaseDefaultList}
+        >
+            <DocumentBodyOverflow>
+                <ProblemPageClient
+                    problemId={id}
+                    testCaseDefaultList={testCaseDefaultList}
+                    programmingLanguageList={programmingLanguageList}
+                />
+            </DocumentBodyOverflow>
+        </ProblemProvider>
     )
 }
